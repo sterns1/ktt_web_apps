f66132ae0b34aecdf5df13ce09132942d0dcb854
Stephen Stern <sterns1@email.arizona.edu>
2020-03-07 15:34:19 -0700

    Added dynamic code to track days, not finished.
    - Made months dynamically get the current day, year, and days.
    - Added a stub for the get_next_month button.
    - Fixed the html charset declaration.
    - Removed a lot of cruft from the original files on the html side.
    
    TODO:
    - Still need to get the arrows working.
    - Need to make sure days show up on the right weekday (March is a bad
        test), but will have to do that after the first bullet is done.
    - Need to add the events from REST gets.

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,6 +1,66 @@
 function load_current_month() {
+  // Updates the current month and year as a starting place for the schedule.
   const date = new Date();
-  const month = date.toLocaleString('default', { month: 'long' });
+  const month = date.toLocaleString('default', { month: 'long' })
   document.getElementById("current_year").innerHTML = date.getFullYear()
   document.getElementById("month_name").innerHTML = month
+}
+
+function load_days() {
+  const current_date = new Date()
+  // Sets the month's start weekday, and number of days.
+  var schedule_days = _get_day_info(
+    current_date.getMonth() + 1, current_date.getFullYear())
+
+  // Actually add list elements of day info.
+  var weekdays = document.getElementById(
+    "weekdays").getElementsByTagName("li");
+  for(day of schedule_days) {
+    var node = document.createElement("LI")
+    node.setAttribute("weekday", weekdays[day.weekday].innerText)
+    var text_node = document.createTextNode(day.number.toString())
+    node.appendChild(text_node)
+    document.getElementById("number_days").appendChild(node)
+  }
+}
+
+function get_next_month() {
+  var displayed_date = _get_displayed_date()
+
+  const current_date = new Date()
+  // Sets the month's start weekday, and number of days.
+  var schedule_days = _get_day_info(displayed_date.month, displayed_date.year)
+
+  // Actually add list elements of day info.
+  var weekdays = document.getElementById(
+    "weekdays").getElementsByTagName("li")
+  for(day of schedule_days) {
+    var node = document.createElement("LI")
+    node.setAttribute("weekday", weekdays[day.weekday].innerText)
+    var text_node = document.createTextNode(day.number.toString())
+    node.appendChild(text_node)
+    document.getElementById("number_days").appendChild(node)
+  }
+}
+
+
+function _get_displayed_date() {
+  var displayed_date = {}
+  return displayed_date
+}
+
+
+function _get_day_info(month, year) {
+  const original_date = new Date(`${year}, ${month}, 1`)
+  var date = new Date(`${year}, ${month}, 1`)
+  var total_days = 1
+  var day_info = {}
+  var schedule_days = []
+  while(date.getMonth() === original_date.getMonth()) {
+    day_info = {"weekday": date.getDay(), "number": total_days}
+    schedule_days.push(day_info)
+    total_days += 1
+    date.setDate(date.getDate() + 1)
+  }
+  return schedule_days
 }
