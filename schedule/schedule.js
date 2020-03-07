28a1a6eb38c0a8406c587dda97300abd73e88a69
Stephen Stern <sterns1@email.arizona.edu>
2020-03-06 17:23:06 -0700

    Added onload function, sets the title container.
    schedule.html
        - Added onload call, made the Title container elements load
            dynamically.
    
    schedule.js
        - Got working, not actually does something.

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,6 +1,6 @@
-<script>
-function get_current_month() {
-  document.getElementById("month_name").innerHTML = "f"
+function load_current_month() {
+  const date = new Date();
+  const month = date.toLocaleString('default', { month: 'long' });
+  document.getElementById("current_year").innerHTML = date.getFullYear()
+  document.getElementById("month_name").innerHTML = month
 }
-
-</script>
